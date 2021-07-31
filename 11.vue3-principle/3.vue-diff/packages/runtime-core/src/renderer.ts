09a1fcf613897f27f05298079619dbf108107520
zijue <460195657@qq.com>
2021-07-31 18:55:23 +0800

    feat: 开始diff算法

--- 11.vue3-principle/3.vue-diff/packages/runtime-core/src/renderer.ts
+++ 11.vue3-principle/3.vue-diff/packages/runtime-core/src/renderer.ts
@@ -68,14 +68,56 @@ export function createRenderer(rendererOptions) { // 不再关心是什么平台
         // 最后将父节点挂载到container上
         hostInsert(el, container);
     };
-    const updateElement = (n1, n2, container) => {
+    const patchProps = (el, oldProps, newProps) => {
+        // 两个循环，第一次循环遍历新属性更新到旧节点上，第二次循环遍历旧属性清空新属性中没有的项
+        if (oldProps !== newProps) {
+            for (let key in newProps) {
+                const prev = oldProps[key];
+                const next = newProps[key];
+                if (prev !== next) {
+                    hostPatchProp(el, key, prev, next);
+                }
+            };
+            for (let key in oldProps) {
+                if (!(key in newProps)) {
+                    hostPatchProp(el, key, oldProps[key], null);
+                }
+            }
+        }
+    };
+    const patchKeyedChildren = (c1, c2, container) => {
+        // diff 算法
+    };
+    const patchChildren = (n1, n2, container) => {
+        const c1 = n1.children;
+        const c2 = n2.children;
+        const prevShapeFlag = n1.shapeFlag;
+        const shapeFlag = n2.shapeFlag;
+        // 1.新子节点是文本，直接新的替换旧的
+        if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+            hostSetElementText(container, c2); // 直接替换
+        } else { // 新子节点是数组
+            if (prevShapeFlag & ShapeFlags.ARRAY_CHILDREN) { // 3.新旧子节点都是数组
+                patchKeyedChildren(c1, c2, container);
+            } else { // 2.新子节点是数组，旧子节点是文本
+                hostSetElementText(container, ''); // 先将父节点清空
+                mountChildren(c2, container); // 然后挂载新的子节点
+            }
+        }
+    };
+    const patchElement = (n1, n2, container) => { // 走到这里说明前后两个元素能够复用
+        let el = n2.el = n1.el; // 复用DOM
 
+        const oldProps = n1.props || {};
+        const newProps = n2.props || {};
+        patchProps(el, oldProps, newProps); // 更新属性
+        patchChildren(n1, n2, el); // 更新子节点；diff算法
     };
     const processElement = (n1, n2, container) => {
         if (n1 == null) {
             mountElement(n2, container);
-        } else {
-            updateElement(n1, n2, container); // diff算法
+        } else { // patch方法中对不同节点进行了处理，能走到这里说明n1、n2是相同节点，只是属性或子节点不同
+            patchElement(n1, n2, container); // diff算法
         }
     };
     const processComponent = (n1, n2, container) => {
@@ -85,7 +127,19 @@ export function createRenderer(rendererOptions) { // 不再关心是什么平台
             updateComponent(n1, n2, container); // 更新组件
         }
     };
+    const isSameVnode = (n1, n2) => {
+        return n1.type === n2.type && n1.key === n2.key;
+    };
+    const unmount = (vnode) => {
+        hostRemove(vnode.el);
+    };
     const patch = (n1, n2, container) => {
+        // 判断n1、n2是否为同一个元素；通过type和key判断
+        if (n1 && !isSameVnode(n1, n2)) { // 如果type和key不一样则直接删除老节点后渲染新节点
+            unmount(n1);
+            n1 = null; // 如果n1为空，则直接重新渲染n2
+        }
+
         const { shapeFlag } = n2; // n2 可能是元素或者组件，不同的类型走不同的处理逻辑
         if (shapeFlag & ShapeFlags.ELEMENT) {
             processElement(n1, n2, container); // 处理元素类型
