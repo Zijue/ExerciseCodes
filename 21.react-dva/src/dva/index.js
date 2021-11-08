2827eddc894af49ef5bc65d107e3c021de82fecd
zijue <460195657@qq.com>
2021-11-08 18:26:21 +0800

    feat: 实现effects副作用

--- 21.react-dva/src/dva/index.js
+++ 21.react-dva/src/dva/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, connect } from 'react-redux';
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import createSagaMiddleware from 'redux-saga';
+import * as sagaEffects from 'redux-saga/effects';
 
 export { connect };
 function dva() {
@@ -40,15 +42,54 @@ function dva() {
             reducers[model.namespace] = reducer;
         }
         //把reducers合并成一个根reducer
-        let combineReducer = combineReducers(reducers);
-        let store = createStore(combineReducer);
-        console.log(store.getState());
+        let combinedReducer = combineReducers(reducers);
+        //从app中获取saga的数组
+        const sagas = getSagas(app);
+        let sagaMiddleware = createSagaMiddleware();
+        // let store = createStore(combinedReducer);
+        let store = applyMiddleware(sagaMiddleware)(createStore)(combinedReducer);
+        window.store = store; //用于开发调试
+        //启动全部的saga
+        sagas.forEach(saga => sagaMiddleware.run(saga));
         ReactDOM.render(
             <Provider store={store}>
                 {app._router()}
             </Provider>, document.querySelector(root));
     }
     return app;
+}
+// function prefixType(type, namespace) {
+//     if (type.indexOf('/') === -1) { //如果没有命名空间，就加上自己的命名空间前缀
+//         return namespace + '/' + type;
+//     } else if (type.split('/')[0] === namespace) {
+//         console.warn(`Warning: [sagaEffects.put] ${type} should not be prefixed with namespace ${namespace}`);
+//     }
+//     return type;
+// }
+function createWatcherSaga(key, effect, model) {
+    return function* () {
+        yield sagaEffects.takeEvery(key, function* (action) {
+            yield effect(action, sagaEffects);
+        });
+    }
+}
+function getSaga(model) {
+    const { effects } = model;
+    return function* () {
+        debugger
+        for (const key in effects) { //key=asyncAdd
+            const watcherSaga = createWatcherSaga(key, effects[key], model);
+            yield sagaEffects.fork(watcherSaga);
+        }
+    }
+}
+function getSagas(app) {
+    let sagas = [];
+    for (let model of app._models) {
+        let saga = getSaga(model);
+        sagas.push(saga);
+    }
+    return sagas;
 }
 function prefix(obj, namespace) {
     let newObj = {};
@@ -60,6 +101,9 @@ function prefix(obj, namespace) {
 function prefixNamespace(model) {
     if (model.reducers) {
         model.reducers = prefix(model.reducers, model.namespace);
+    }
+    if (model.effects) {
+        model.effects = prefix(model.effects, model.namespace);
     }
     return model;
 }
--- 21.react-dva/src/index.js
+++ 21.react-dva/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import dva, { connect } from './dva';
 
+const delay = (ms) => new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
 const app = dva();
 app.model({
   namespace: 'counter', //因为一个dvaApp里可以定义很多的模型
@@ -11,36 +14,40 @@ app.model({
     },
     minus(state) {
       return { number: state.number - 1 };
+    },
+    asyncAdd(state) {
+      console.log('reducers asyncAdd');
+      return state;
+    }
+  },
+  effects: {
+    //store.dispatch(action)  redux-saga/effects
+    //核心实现：等待counter/asyncAdd派发后会执行此saga
+    // yield takeEvery('counter/asyncAdd', *asyncAdd);
+    *asyncAdd(action, { call, put }) { //workerSaga
+      console.log('effects asyncAdd');
+      yield call(delay, 1000);
+      //Warning: [sagaEffects.put] counter/add should not be prefixed with namespace counter
+      //如果你在effects里通过put这个effect向仓库派发动作的话，是不需要加命名空间前缀的
+      //如果不写前缀{type: 'add'}，就指的是向自己的命名空间下发送动作，等同于{type: 'counter/add'}
+      yield put({ type: 'counter/add' }); //向仓库派发动作{type: 'counter/add'}
     }
   }
 });
-/**
-const actionCreators = {
-  add() {
-    return { type: 'counter/add' };
-  },
-  minus() {
-    return { type: 'counter/minus' };
-  }
-};
- */
-const actionCreators = app.createAction();
-console.log(actionCreators);
 function Counter(props) {
   return (
     <div>
       <p>{props.number}</p>
-      <button onClick={props['counter/add']}>+</button>
-      <button onClick={props['counter/minus']}>-</button>
-      {/* <button onClick={() => props.dispatch({ type: 'counter/add' })}>+</button>
-      <button onClick={() => props.dispatch({ type: 'counter/minus' })}>-</button> */}
+      <button onClick={() => props.dispatch({ type: 'counter/add' })}>+</button>
+      <button onClick={() => props.dispatch({ type: 'counter/minus' })}>-</button>
+      <button onClick={() => props.dispatch({ type: 'counter/asyncAdd' })}>asyncAdd</button>
     </div>
   )
 }
 //store.getState() => { counter: { number: 0 } }
 const mapStateToProps = state => state.counter;
 //connect中不传入actionCreators，会绑定store.dispatch方法
-const ConnentedCounter = connect(mapStateToProps, actionCreators)(Counter);
+const ConnentedCounter = connect(mapStateToProps)(Counter);
 //指定要渲染的内容
 app.router(() => <ConnentedCounter />)
 //开始渲染
